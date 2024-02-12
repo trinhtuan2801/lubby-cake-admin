6f97b87fbfd5b7d8b8d6436cc92fcecdaae74cd1
Trinh Tuan <trinhtuan2801@gmail.com>
2024-02-12 10:27:36 +0700

    init add cake modal

---
+++ src/pages/cake/CakeTable/AddCakeModal.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  open: boolean;
+  onClose: () => void;
+}
+
+// eslint-disable-next-line
+export default function AddCakeModal({ open, onClose }: Props) {
+  // eslint-disable-next-line
+  return <></>;
+}
