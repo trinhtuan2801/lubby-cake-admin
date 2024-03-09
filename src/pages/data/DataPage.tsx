1908ba3ad81216100f01cd76ac27578924daa57e
DESKTOP-J743OV8\local user <trinhtuan2801@gmail.com>
2024-03-10 03:16:14 +0700

    deploy hook

--- src/pages/data/DataPage.tsx
+++ src/pages/data/DataPage.tsx
@@ -1,7 +1,9 @@
+import MyModal from '@/components/MyModal/MyModal';
 import { deleteUnusedImages } from '@/firebase/utils';
-import { HideImageOutlined } from '@mui/icons-material';
+import { Backup, HideImageOutlined } from '@mui/icons-material';
 import { Box, Button, Typography } from '@mui/joy';
 import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 
 export default function DataPage() {
@@ -15,12 +17,14 @@ export default function DataPage() {
     },
   });
 
+  const [openConfirmUpdate, setOpenConfirmUpdate] = useState(false);
+
   return (
     <>
       <Typography level='title-md' color='primary' fontWeight='bold'>
         Dữ liệu
       </Typography>
-      <Box>
+      <Box display='flex' flexDirection='column' gap={1} width='fit-content'>
         <Button
           startDecorator={<HideImageOutlined />}
           onClick={() => deleteUnusedImagesMT.mutate()}
@@ -28,6 +32,37 @@ export default function DataPage() {
         >
           Xóa ảnh không dùng
         </Button>
+
+        <Button
+          startDecorator={<Backup />}
+          onClick={() => setOpenConfirmUpdate(true)}
+          color='danger'
+        >
+          Cập nhật dữ liệu bánh
+        </Button>
+
+        <MyModal
+          open={openConfirmUpdate}
+          onClose={() => setOpenConfirmUpdate(false)}
+          OkButtonLabel='Cập nhật'
+          title={
+            <Typography color='danger' fontWeight='bold'>
+              Xác nhận
+            </Typography>
+          }
+          OkButtonProps={{
+            color: 'danger',
+          }}
+          onOk={() => {
+            fetch(
+              'https://api.vercel.com/v1/integrations/deploy/prj_9tvC44t5Ck1ZBfMgzceqL6b0655D/eDM2K18NZs',
+            );
+            toast.success('Đang cập nhật dữ liệu...');
+            setOpenConfirmUpdate(false);
+          }}
+        >
+          <Typography>Cập nhật dữ liệu sẽ mất khoảng 1 phút</Typography>
+        </MyModal>
       </Box>
     </>
   );
